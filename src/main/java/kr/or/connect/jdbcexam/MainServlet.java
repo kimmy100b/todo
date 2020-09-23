e3d1ad08129d9cfff3a2d0057d8703aa86c40967
kimmy100b <kimmy100b@gmail.com>
2020-09-23 17:08:06 +0900

    feat : BE - 할일목록 UI 구현
    issue : #9

--- src/main/java/kr/or/connect/jdbcexam/MainServlet.java
+++ src/main/java/kr/or/connect/jdbcexam/MainServlet.java
@@ -14,9 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public MainServlet() {
         super();
         // TODO Auto-generated constructor stub
